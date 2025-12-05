66c28f53b66bacab8d6065248ff44fbcdd5db140
mokeYQL <1318284339@qq.com>
2025-12-05 11:12:42 +0800

    复刻项目从0开始

---
+++ products/Replica_ai/main/hello_world_main.c
@@ -0,0 +1,14 @@
+/*
+ * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#include <stdio.h>
+
+void app_main(void)
+{
+    printf("Hello world!\n");
+    printf("Hello world 2 !\n");
+    printf("Hello world 3 !\n");
+}
