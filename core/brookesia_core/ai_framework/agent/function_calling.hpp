0e93260078cc661d27efe7b777fdc8f30fa2a116
mokeYQL <1318284339@qq.com>
2025-12-12 17:46:54 +0800

    调试阶段  避免  每次烧录 .aaf 图像文件

--- core/brookesia_core/ai_framework/agent/function_calling.hpp
+++ core/brookesia_core/ai_framework/agent/function_calling.hpp
@@ -14,105 +14,109 @@
 #include "cJSON.h"
 #include "thread/esp_utils_thread.hpp"
 
-namespace esp_brookesia::ai_framework {
+namespace esp_brookesia::ai_framework
+{
 
-class FunctionParameter {
-public:
-    enum class ValueType {
-        Boolean,
-        Number,
-        String
+    class FunctionParameter
+    {
+    public:
+        enum class ValueType
+        {
+            Boolean,
+            Number,
+            String
+        };
+
+        FunctionParameter(const std::string &name, const std::string &description, ValueType type, bool required = true);
+
+        const std::string &name() const
+        {
+            return name_;
+        }
+        const std::string &description() const
+        {
+            return description_;
+        }
+        ValueType type() const
+        {
+            return type_;
+        }
+        bool required() const
+        {
+            return required_;
+        }
+
+        bool boolean() const
+        {
+            return boolean_;
+        }
+        int number() const
+        {
+            return number_;
+        }
+        const std::string &string() const
+        {
+            return string_;
+        }
+
+        void setBoolean(bool value);
+        void setNumber(int value);
+        void setString(const std::string &value);
+
+        std::string getDescriptorJson();
+
+    private:
+        std::string name_;
+        std::string description_;
+        ValueType type_;
+        bool required_;
+        bool boolean_;
+        int number_;
+        std::string string_;
     };
 
-    FunctionParameter(const std::string &name, const std::string &description, ValueType type, bool required = true);
-
-    const std::string &name() const
+    class FunctionDefinition
     {
-        return name_;
-    }
-    const std::string &description() const
+    public:
+        using CallbackThreadConfig = esp_utils::ThreadConfig;
+        using Callback = std::function<void(const std::vector<FunctionParameter> &)>;
+
+        FunctionDefinition(const std::string &name, const std::string &description);
+
+        void addParameter(
+            const std::string &name, const std::string &description, FunctionParameter::ValueType type, bool required = true);
+        void setCallback(Callback callback, std::optional<CallbackThreadConfig> thread_config = std::nullopt);
+        bool invoke(const cJSON *args) const;
+        std::string name() const
+        {
+            return name_;
+        }
+        std::string getJson() const;
+
+    private:
+        std::string name_;
+        std::string description_;
+        std::vector<FunctionParameter> parameters_;
+        Callback callback_;
+        std::optional<CallbackThreadConfig> thread_config_;
+    };
+
+    class FunctionDefinitionList
     {
-        return description_;
-    }
-    ValueType type() const
-    {
-        return type_;
-    }
-    bool required() const
-    {
-        return required_;
-    }
+    public:
+        FunctionDefinitionList(const FunctionDefinitionList &) = delete;
+        FunctionDefinitionList &operator=(const FunctionDefinitionList &) = delete;
 
-    bool boolean() const
-    {
-        return boolean_;
-    }
-    int number() const
-    {
-        return number_;
-    }
-    const std::string &string() const
-    {
-        return string_;
-    }
+        static FunctionDefinitionList &requestInstance();
+        void addFunction(const FunctionDefinition &func);
+        bool invokeFunction(const cJSON *function_call) const;
+        std::string getJson() const;
 
-    void setBoolean(bool value);
-    void setNumber(int value);
-    void setString(const std::string &value);
+    private:
+        FunctionDefinitionList() = default;
 
-    std::string getDescriptorJson();
-
-private:
-    std::string name_;
-    std::string description_;
-    ValueType type_;
-    bool required_;
-    bool boolean_;
-    int number_;
-    std::string string_;
-};
-
-class FunctionDefinition {
-public:
-    using CallbackThreadConfig = esp_utils::ThreadConfig;
-    using Callback = std::function<void(const std::vector<FunctionParameter>&)>;
-
-    FunctionDefinition(const std::string &name, const std::string &description);
-
-    void addParameter(
-        const std::string &name, const std::string &description, FunctionParameter::ValueType type, bool required = true
-    );
-    void setCallback(Callback callback, std::optional<CallbackThreadConfig> thread_config = std::nullopt);
-    bool invoke(const cJSON *args) const;
-    std::string name() const
-    {
-        return name_;
-    }
-    std::string getJson() const;
-
-private:
-    std::string name_;
-    std::string description_;
-    std::vector<FunctionParameter> parameters_;
-    Callback callback_;
-    std::optional<CallbackThreadConfig> thread_config_;
-};
-
-class FunctionDefinitionList {
-public:
-    FunctionDefinitionList(const FunctionDefinitionList &) = delete;
-    FunctionDefinitionList &operator=(const FunctionDefinitionList &) = delete;
-
-    static FunctionDefinitionList &requestInstance();
-    void addFunction(const FunctionDefinition &func);
-    bool invokeFunction(const cJSON *function_call) const;
-    std::string getJson() const;
-
-private:
-    FunctionDefinitionList() = default;
-
-    std::vector<FunctionDefinition> functions_;
-    std::map<std::string, size_t> function_index_;
-};
+        std::vector<FunctionDefinition> functions_;
+        std::map<std::string, size_t> function_index_;
+    };
 
 } // namespace esp_brookesia::ai_framework
