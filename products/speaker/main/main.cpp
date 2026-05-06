b634da9b0f1ea061a166f98b70232d2573107ddd
mokeYQL <1318284339@qq.com>
2026-05-06 18:18:26 +0800

    chore: 更新版权年份并调整代码格式

--- products/speaker/main/main.cpp
+++ products/speaker/main/main.cpp
@@ -7,7 +7,7 @@
 #include "lvgl.h"
 #include "boost/thread.hpp"
 #ifdef ESP_UTILS_LOG_TAG
-#   undef ESP_UTILS_LOG_TAG
+#undef ESP_UTILS_LOG_TAG
 #endif
 #define ESP_UTILS_LOG_TAG "Main"
 #include "esp_lib_utils.h"
@@ -19,36 +19,39 @@
 #include "modules/file_system.hpp"
 #include "modules/led_indicator.h"
 
-constexpr bool EXAMPLE_SHOW_MEM_INFO = false;
+constexpr bool EXAMPLE_SHOW_MEM_INFO = true;
 
 extern "C" void app_main()
 {
     restart_usb_serial_jtag();
     printf("Project version: %s\n", CONFIG_APP_PROJECT_VER);
 
-    assert(services_init()                      && "Initialize services failed");
+    assert(services_init() && "Initialize services failed");
     auto default_dummy_draw = !system_check_is_developer_mode();
-    assert(display_init(default_dummy_draw)     && "Initialize display failed");
-    assert(led_indicator_init()                 && "Initialize led indicator failed");
-    if (!file_system_init()) {
+    assert(display_init(default_dummy_draw) && "Initialize display failed");
+    assert(led_indicator_init() && "Initialize led indicator failed");
+    if (!file_system_init())
+    {
         ESP_UTILS_LOGE("Initialize file system failed, related features will be disabled");
     }
-    assert(audio_init()                         && "Initialize audio failed");
-    assert(system_init()                        && "Initialize system failed");
+    assert(audio_init() && "Initialize audio failed");
+    assert(system_init() && "Initialize system failed");
 
-    if constexpr (EXAMPLE_SHOW_MEM_INFO) {
+    if constexpr (EXAMPLE_SHOW_MEM_INFO)
+    {
         esp_utils::thread_config_guard thread_config({
             .name = "mem_info",
             .stack_size = 4096,
         });
-        boost::thread([ = ]() {
+        boost::thread([=]()
+                      {
             while (1) {
                 esp_utils_mem_print_info();
 
                 audio_sys_get_real_time_stats();
 
                 boost::this_thread::sleep_for(boost::chrono::seconds(5));
-            }
-        }).detach();
+            } })
+            .detach();
     }
 }
